594ab44ba54d459d29ce7df5e2dc4a0225200c98
Hugo Machado <hugo_machado02@hotmail.com>
2025-12-05 13:27:11 -0300

    ♻️ refactor: Limpeza de código
    Realiza a refatoração do código removendo os cógdigos desnecessários

--- lib/models/game.dart
+++ lib/models/game.dart
@@ -1,15 +1,11 @@
-import 'package:hive/hive.dart';
-
-@HiveType(typeId: 0)
 class Game {
-  Game({required this.id, required this.title, required this.urlImage});
+  final int id;
+  final String title;
+  final String urlImage;
 
-  @HiveField(0)
-  int id;
-
-  @HiveField(1)
-  String title;
-
-  @HiveField(2)
-  String urlImage;
+  Game({
+    required this.id,
+    required this.title,
+    required this.urlImage,
+  });
 }

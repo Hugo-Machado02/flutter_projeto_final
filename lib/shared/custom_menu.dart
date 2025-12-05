b953701266dabb98b3ee7a91676d3e5fe88a7180
Hugo Machado <hugo_machado02@hotmail.com>
2025-12-05 02:24:06 -0300

    ✨ feat: Implementa GetX na navegação do Menu

--- lib/shared/custom_menu.dart
+++ lib/shared/custom_menu.dart
@@ -1,28 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:projeto_final/controllers/navigation_controller.dart';
 
 class CustomMenu extends StatelessWidget {
   const CustomMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      backgroundColor: Color(0xFF141518),
+    final NavigationController navController = Get.find<NavigationController>();
+    
+    return Obx(() => BottomNavigationBar(
+      backgroundColor: const Color(0xFF141518),
+      selectedItemColor: const Color(0xFF8162FF),
+      unselectedItemColor: Colors.white70,
+      type: BottomNavigationBarType.fixed,
+      elevation: 0,
+      currentIndex: navController.currentIndex.value,
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Usuários'),
         BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'favoritos'),
+        BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favoritos'),
       ],
-      selectedItemColor: Color(0xFF8162FF),
-      unselectedItemColor: Colors.white,
-      onTap: (int index) {
-        if (index == 1) {
-          Navigator.pushReplacementNamed(context, '/Home');
-        } else if (index == 0) {
-          Navigator.pushReplacementNamed(context, '/usuarios');
-        } else if (index == 2) {
-          Navigator.pushReplacementNamed(context, '/favoritos');
-        }
-      },
-    );
+      onTap: navController.changeTab,
+    ));
   }
 }

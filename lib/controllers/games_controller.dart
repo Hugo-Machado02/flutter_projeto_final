e877169aff1d2aa2940670550711ddb105114b5d
Hugo Machado <hugo_machado02@hotmail.com>
2025-12-05 02:44:33 -0300

    ♻️ refactor: Refatora snackbar dos Games

--- lib/controllers/games_controller.dart
+++ lib/controllers/games_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:projeto_final/models/game.dart';
 
@@ -18,52 +19,70 @@ class GamesController extends GetxController {
       Game(
         id: 1,
         title: "God Of War",
-        urlImage: "https://media.rawg.io/media/games/52b/52be96c19be0d79eeb8664e4ca2aabd5.jpg",
+        urlImage:
+            "https://media.rawg.io/media/games/52b/52be96c19be0d79eeb8664e4ca2aabd5.jpg",
       ),
       Game(
         id: 2,
         title: "The Last of Us",
-        urlImage: "https://media.rawg.io/media/games/6fc/6fcf4cd3b17c288821388e6085bb0fc9.jpg",
+        urlImage:
+            "https://media.rawg.io/media/games/6fc/6fcf4cd3b17c288821388e6085bb0fc9.jpg",
       ),
       Game(
         id: 3,
         title: "Red Dead Redemption 2",
-        urlImage: "https://media.rawg.io/media/games/52b/52be96c19be0d79eeb8664e4ca2aabd5.jpg",
+        urlImage:
+            "https://media.rawg.io/media/games/52b/52be96c19be0d79eeb8664e4ca2aabd5.jpg",
       ),
       Game(
         id: 4,
         title: "Cyberpunk 2077",
-        urlImage: "https://media.rawg.io/media/games/6fc/6fcf4cd3b17c288821388e6085bb0fc9.jpg",
+        urlImage:
+            "https://media.rawg.io/media/games/6fc/6fcf4cd3b17c288821388e6085bb0fc9.jpg",
       ),
       Game(
         id: 5,
         title: "Elden Ring",
-        urlImage: "https://media.rawg.io/media/games/b54/b54598d1d5cc31899f4f0a7e3122a7b0.jpg",
+        urlImage:
+            "https://media.rawg.io/media/games/b54/b54598d1d5cc31899f4f0a7e3122a7b0.jpg",
       ),
     ];
   }
 
   List<Game> get filteredGames {
     if (searchText.value.isEmpty) {
-      return games;
+      return games.value;
     }
-    return games.where((game) => 
-      game.title.toLowerCase().contains(searchText.value.toLowerCase())
-    ).toList();
+    return games.value
+        .where(
+          (game) =>
+              game.title.toLowerCase().contains(searchText.value.toLowerCase()),
+        )
+        .toList();
   }
 
   void toggleFavorite(Game game) {
     if (isFavorite(game)) {
-      favoriteGames.removeWhere((g) => g.id == game.id);
-      Get.snackbar('Removido', '${game.title} removido dos favoritos');
+      favoriteGames.removeWhere((item) => item.id == game.id);
+      Get.snackbar(
+        'Favoritos',
+        '${game.title} removido dos favoritos',
+        backgroundColor: const Color(0xFF8162FF),
+        colorText: Colors.white,
+      );
     } else {
       favoriteGames.add(game);
-      Get.snackbar('Adicionado', '${game.title} adicionado aos favoritos');
+      Get.snackbar(
+        'Favoritos',
+        '${game.title} adicionado aos favoritos',
+        backgroundColor: const Color(0xFF8162FF),
+        colorText: Colors.white,
+      );
     }
   }
 
   bool isFavorite(Game game) {
-    return favoriteGames.any((g) => g.id == game.id);
+    return favoriteGames.any((item) => item.id == game.id);
   }
 
   void searchGames(String query) {

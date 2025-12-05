3768c06d1a8531f5659a92a2b2ab1d12eab39106
Hugo Machado <hugo_machado02@hotmail.com>
2025-12-05 02:28:37 -0300

    ✨ feat: Implementa getX no Main  com as rotas

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:projeto_final/pages/list_games.dart';
 import 'package:projeto_final/pages/favorite_games.dart';
 import 'package:projeto_final/pages/list_users.dart';
+import 'package:projeto_final/pages/login.dart';
+import 'package:projeto_final/controllers/auth_controller.dart';
+import 'package:projeto_final/controllers/games_controller.dart';
+import 'package:projeto_final/controllers/navigation_controller.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,18 +17,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    // Inicializar controllers
+    Get.put(AuthController());
+    Get.put(GamesController());
+    Get.put(NavigationController());
+    
+    return GetMaterialApp(
       title: 'Catálogo de Jogos',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF8162FF)),
       ),
-      routes: {
-        '/usuarios': (context) => const ListUsers(),
-        '/Home': (context) => const ListGames(),
-        '/favoritos': (context) => const FavoriteGames(),
-      },
-
-      home: const ListGames(),
+      initialRoute: '/login',
+      getPages: [
+        GetPage(name: '/login', page: () => const Login()),
+        GetPage(name: '/home', page: () => const ListGames()),
+        GetPage(name: '/usuarios', page: () => const ListUsers()),
+        GetPage(name: '/favoritos', page: () => const FavoriteGames()),
+      ],
     );
   }
 }
